35f84d7394c70475a378e7bb3975e02796cbc317
arkajitroy <arkajitroy11.official@gmail.com>
2025-06-02 21:28:47 +0530

    feat: add initial account-details page

--- src/app/(secured)/account/[id]/page.tsx
+++ src/app/(secured)/account/[id]/page.tsx
@@ -1,5 +1,16 @@
+import { getAccountWithTransactions } from "@/features/account/server/action";
 import React from "react";
 
-export default function AccountPage() {
+type TAccountPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function AccountPage({ params }: TAccountPageProps) {
+  const accountData = await getAccountWithTransactions(params.id);
+
+  console.log("debug:accountData", accountData);
+
   return <div>Account Page</div>;
 }
