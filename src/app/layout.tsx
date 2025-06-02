219d0accc4de96800842bd498665374307d6d6df
arkajitroy <arkajitroy11.official@gmail.com>
2025-06-02 13:37:34 +0530

    feat: add shadcn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
-import Navbar from "@/components/shared/navbar";
-import Footer from "@/components/shared/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,11 +29,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          {/* Navbar */}
-          <Navbar />
           {children}
-          {/* Footer */}
-          <Footer />
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import HomePage from "./(landing)";
+import Navbar from "@/components/shared/navbar";
+import Footer from "@/components/shared/footer";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <>
+      <Navbar />
+      <HomePage />;
+      <Footer />
+    </>
+  );
 }
